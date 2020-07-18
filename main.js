982010b2a4c253cbc1913bb8b3639190b247ed2a
Štěpán Zoubek <stepan.zoubek@gmail.com>
2020-07-18 23:24:47 +0200

    Menu structure add

--- main.js
+++ main.js
@@ -1,12 +1,51 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
+
+//Set env
+process.env.NODE_ENV = 'development'
+const isDev = process.env.NODE_ENV !== 'production' ? true : false
+const isMac = process.platform === 'linux' ? true : false
+
+
+
 let mainWindow
-function createMainWindow(){
-     mainWindow = new BrowserWindow({
-        title:'ImageShrink',
-        width:500,
-        height:600
+function createMainWindow() {
+    mainWindow = new BrowserWindow({
+        title: 'ImageShrink',
+        width: 500,
+        height: 600,
+        icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+        resizable: isDev
     })
-    mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+
+    mainWindow.loadFile('./app/index.html')
 }
 
-app.on('ready', createMainWindow)
+
+app.on('window-all-closed', () => {
+    if (!isMac) {
+        app.quit
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createMainWindow()
+    }
+})
+
+const menu = [{
+    label:'File',
+    submenu:[
+        {
+            label:'Quit',
+            click:()=>app.quit()
+        }
+    ]
+}]
+
+app.on('ready', () => {
+    createMainWindow()
+    const mainMenu = Menu.buildFromTemplate(menu)
+    Menu.setApplicationMenu(mainMenu)
+    mainWindow.on('closed', () => mainWindow = null)
+})
