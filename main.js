9391a70199cb5af42b236cf63a6297b797a3caf6
Štěpán Zoubek <stepan.zoubek@gmail.com>
2020-07-20 13:59:19 +0200

    Add imagemin dependecies, functional shrinking of image

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-const { app, BrowserWindow, Menu, ipcMain, } = require('electron')
-
+const path = require('path')
+const os = require('os')
+const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron')
+const imagemin = require('imagemin')
+const imageminMozjpeg = require('imagemin-mozjpeg')
+const imageminPngquant = require('imagemin-pngquant')
+const slash = require('slash')
 //Set env
 process.env.NODE_ENV = 'development'
 const isDev = process.env.NODE_ENV !== 'production' ? true : false
@@ -92,9 +97,30 @@ const menu = [
     ] : [])
 ]
 
-ipcMain.on('image:minimize', (e,options)=>{
-console.log(options)
+ipcMain.on('image:minimize', (e, options) => {
+    options.dest = path.join(os.homedir(), 'imageshrink'),
+        shrinkImage(options)
 })
+
+async function shrinkImage({ imgPath, quality, dest }) {
+    try {
+        const pngQuality = quality / 100
+        const files = await imagemin([slash(imgPath)], {
+            destination: dest,
+            plugins: [imageminMozjpeg({ quality }),
+            imageminPngquant({
+                quality: [pngQuality, pngQuality]
+            })
+            ]
+
+        })
+        console.log(files)
+        shell.openPath(dest)
+    } catch (error) {
+        console.log(error)
+    }
+
+}
 
 app.on('ready', () => {
     createMainWindow()
