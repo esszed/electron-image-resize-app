97d72f7bf4de6b0334a20a0d3246e08de5309f0c
Štěpán Zoubek <stepan.zoubek@gmail.com>
2020-07-20 13:39:09 +0200

    Get path of the image

--- app/js/render.js
+++ app/js/render.js
@@ -1,5 +1,23 @@
 const path = require('path')
 const os = require('os')
+const { ipcRenderer } = require('electron')
 
 
-document.getElementById('output-path').innerText=path.join(os.homedir(),'imageshrink')
+const form = document.getElementById('image-form')
+const slider = document.getElementById('slider')
+const img = document.getElementById('img')
+
+
+//Onsubmit
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const imgPath = img.files[0].path
+    const quality = slider.value
+    ipcRenderer.send('image:minimize', {
+        imgPath,
+        quality,
+    })
+})
+
+document.getElementById('output-path').innerText = path.join(os.homedir(), 'imageshrink')
